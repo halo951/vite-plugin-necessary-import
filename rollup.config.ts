b4e35d7d997c6cf1a3e52183099e7847fa3bc28d
Halo <1654403734@qq.com>
2023-03-14 10:30:30 +0800

    fix: 更正rollup构建配置

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,26 +1,34 @@
 import typescript from 'rollup-plugin-typescript2'
+import { terser } from 'rollup-plugin-terser'
 import { RollupOptions } from 'rollup'
-import pkg from './package.json'
-import * as np from 'path'
+import * as fs from 'node:fs'
+
+const pkg = JSON.parse(fs.readFileSync('package.json', { encoding: 'utf-8' }))
 
 const banner: string = `
 /** ${pkg.name}
  *
- * @author ${pkg.author.name} (${pkg.author.url})
+ * @author ${pkg.author.name}(${pkg.author.url})
  * @license ${pkg.license}
  */`.trim()
 
 /** export rollup.config */
-export default (): RollupOptions => {
-    const formats: Array<'cjs' | 'es'> = ['cjs', 'es']
+export default async (): Promise<RollupOptions | Array<RollupOptions>> => {
     return {
+        treeshake: false,
+        strictDeprecations: false,
         input: 'src/index.ts',
-        plugins: [typescript({ clean: true, useTsconfigDeclarationDir: true, abortOnError: true })],
-        output: formats.map((f) => ({
-            format: f,
-            dir: np.join('dist', f),
+        plugins: [
+            typescript({ clean: true, useTsconfigDeclarationDir: true, abortOnError: true }),
+            // compress
+            terser()
+        ],
+        output: {
             exports: 'auto',
-            banner
-        }))
+            inlineDynamicImports: true,
+            banner,
+            format: 'cjs',
+            file: 'dist/index.js'
+        }
     }
 }
