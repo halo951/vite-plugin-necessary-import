8fca4a0b9a7fcccedb1fe7336e2a182e2634c57a
Halo <1654403734@qq.com>
2023-03-14 09:53:32 +0800

    chore: 更新rollup版本配置

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,5 +1,5 @@
 import typescript from 'rollup-plugin-typescript2'
-import { GetManualChunkApi, RollupOptions } from 'rollup'
+import { RollupOptions } from 'rollup'
 import pkg from './package.json'
 import * as np from 'path'
 
@@ -20,23 +20,7 @@ export default (): RollupOptions => {
             format: f,
             dir: np.join('dist', f),
             exports: 'auto',
-            banner,
-            manualChunks: (id: string, api: GetManualChunkApi): string => {
-                const abstractPath: string = np.relative(np.join(process.cwd(), 'src'), id)
-                if (/^configs\\/.test(abstractPath)) {
-                    return np.join('config', np.basename(id, '.ts'))
-                }
-                if (/rules\\/.test(abstractPath)) {
-                    return 'rules'
-                }
-                if (/utils\\/.test(abstractPath)) {
-                    return 'utils'
-                }
-                return `index`
-            },
-            chunkFileNames: (chunk) => {
-                return chunk.name + '.js'
-            }
+            banner
         }))
     }
 }
